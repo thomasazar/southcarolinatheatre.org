11131f3fe6a2b36aead76c5d23658cb92de4deda
Thomas Azar <design@thomasazar.net>
2016-02-10 14:24:16 -0500

    Use native WordPress menus for header navigation

--- content/themes/DesignByThomasAzar/functions.php
+++ content/themes/DesignByThomasAzar/functions.php
@@ -1,2 +1,8 @@
 <?php
+require_once( 'lib/admin/menus.php' );
+
 require_once( 'lib/clean-wp-list-pages.php' );
+
+require_once( 'lib/post-types/divisions.php' );
+
+?>
--- content/themes/DesignByThomasAzar/nav.php
+++ content/themes/DesignByThomasAzar/nav.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 $nav_walker      = new Clean_Walker();
 
 $args = array(
@@ -11,5 +11,14 @@ $args = array(
 $navMenu = wp_list_pages( $args );
 echo '<div class=\'mobile-only menu-open-button\' id=\'mobile-menu\'>Menu</div><nav class=\'header-menu\'>	<ul class=\'header-menu-items\' id=\'headerMenuItems\'>	<li class=\'menu-item menu-close-button mobile-only\' id=\'menu-close-button\'><a href=\'#\'>Close</a></li>';
 echo $navMenu;
-
+*/
 ?>
+<?php
+wp_nav_menu( array(
+	'theme_location'  => 'header-menu',
+	'menu_class'	  => 'header-menu-items',
+	'container'		  => 'nav',
+	'container_class' => 'header-menu',
+	
+ ) );
+ ?>
