936c83196ad917ae088cd08c1cb40fe5805440ae
Thomas Azar <design@thomasazar.net>
2016-02-10 10:50:12 -0500

    Move header to its own file

--- content/themes/DesignByThomasAzar/page.php
+++ content/themes/DesignByThomasAzar/page.php
@@ -1,29 +1,7 @@
 <?php get_header(); ?>
 <body>
 	<header class='header'>
-		<div class='header-logo-container'>
-			<img src="/img/scta-logo.jpg" />
-			<p class='tagline'>Connect. Create. Cultivate.</p>
-			<div id="timer">
-				<p>Gilded Gala Countdown</p>
-				<div class='timer__box'>
-					<span class="timer__days"></span>
-					<div class="timer__smalltext">Days</div>
-				</div>
-				<div class='timer__box'>
-					<span class="timer__hours"></span>
-					<div class="timer__smalltext">Hours</div>
-				</div>
-				<div class='timer__box'>
-					<span class="timer__minutes"></span>
-					<div class="timer__smalltext">Min</div>
-				</div>
-				<div class='timer__box'>
-					<span class="timer__seconds"></span>
-					<div class="timer__smalltext">Sec</div>
-				</div>
-			</div>
-		</div>
+		<?php get_template_part( 'page-header' ); ?>
 		<?php get_template_part( 'nav' ); ?>
 	</header>
 	<main class='container'>
