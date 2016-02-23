b3a37ddd912d5689a97e7c01a2ada4d3bf4a53b1
Thomas Azar <design@thomasazar.net>
2016-02-23 13:11:30 -0500

    If on a page with children, show the children as links at the top of the page.

--- content/themes/DesignByThomasAzar/page.php
+++ content/themes/DesignByThomasAzar/page.php
@@ -9,6 +9,19 @@
 			<?php while ( have_posts() ) : the_post(); ?>
 			<h1 class='post-title'><?php the_title(); ?></h1>
 			<section class='post__content'>
+				<?php
+				$child_pages = get_pages( array( 'child_of' => $post->ID ) );
+				if ( $child_pages ){
+					$output = '<div class=\'download\'>';
+					foreach ( $child_pages as $child_page ) {
+						$title = $child_page->post_title;
+						$href  = get_permalink( $child_page->ID );
+						$output .= "<a href='$href' class='button category'>$title</a>";
+					}
+					$output .= '</div>';
+					echo $output;
+				}
+				?>
 				<?php the_content(); ?>
 			</section>
 			<?php endwhile; ?>
