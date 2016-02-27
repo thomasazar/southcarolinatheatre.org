2f580b42524e38b4a87202b349ec871a50cda334
Thomas Azar <design@thomasazar.net>
2016-02-27 14:36:58 -0500

    Move 'Search on SCTA' to placeholder text

--- content/themes/DesignByThomasAzar/searchform.php
+++ content/themes/DesignByThomasAzar/searchform.php
@@ -10,6 +10,5 @@
 			<svg viewBox="0 0 16 16" width="24px" height="24px" preserveAspectRatio="xMinYMin meet" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414"><g><path d="M5.09 7.273v1.745H7.98c-.116.75-.873 2.197-2.887 2.197-1.737 0-3.155-1.44-3.155-3.215S3.353 4.785 5.09 4.785c.99 0 1.652.422 2.03.786l1.382-1.33c-.887-.83-2.037-1.33-3.41-1.33C2.275 2.91 0 5.184 0 8s2.276 5.09 5.09 5.09c2.94 0 4.888-2.065 4.888-4.974 0-.334-.036-.59-.08-.843H5.09zM16 7.273h-1.455V5.818H13.09v1.455h-1.454v1.454h1.455v1.455h1.455V8.727H16" style="fill:#dc4e41;fill-opacity:1" /></g></svg>
 		</a>
 	</div>
-    <label for="search">Search on SCTA</label>
-    <input type="text" name="s" id="search" value="<?php the_search_query(); ?>" /><button class='search-submit' type='submit'>Search</button>
+    <input type="text" name="s" placeholder="Search on SCTA" value="<?php the_search_query(); ?>" /><button class='search-submit' type='submit'>Search</button>
 </form>
