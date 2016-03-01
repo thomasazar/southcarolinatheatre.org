16cef9af82c6b379aec442812c6f1f17a2132557
Thomas Azar <design@thomasazar.net>
2016-03-01 15:51:11 -0500

    Use clean walker in navigation

--- content/themes/DesignByThomasAzar/nav.php
+++ content/themes/DesignByThomasAzar/nav.php
@@ -1,29 +1,16 @@
-<?php
-/*
-$nav_walker      = new Clean_Walker();
-
-$args = array(
-	'title_li' => '',
-	'echo' => 0,
-	'walker' => $nav_walker,
-);
-
-$navMenu = wp_list_pages( $args );
-echo '<div class='mobile-only menu-open-button' id='mobile-menu'>Menu</div><nav class='header-menu'>	<ul class='header-menu-items' id='headerMenuItems'>	<li class='menu-item menu-close-button mobile-only' id='menu-close-button'><a href='#'>Close</a></li>';
-echo $navMenu;
-*/
-?>
-<div class='mobile-only menu-open-button' id='mobile-menu'>Menu</div>
+<div class='button mobile-only mobile-menu'>Menu</div>
 <nav class="header-menu">
-	<ul class='header-menu-items' id='headerMenuItems'>
-		<li class='menu-item menu-close-button mobile-only' id='menu-close-button'>
-			<a href='#'>Close</a>
-		</li>
+	<div class='mobile-only' style='padding: 1rem 0;'>
+		<?php get_search_form(); ?>
+	</div>
+	<ul class='header-menu-items'>
+		</div>
 		<?php
 
 		$args = array(
 			'title_li' => '',
 			'depth'    => '2',
+			'walker'   => new Clean_Walker_Page,
 		);
 
 		wp_list_pages( $args );
