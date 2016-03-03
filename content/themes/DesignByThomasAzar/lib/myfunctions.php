f3a7774b59521f039e8a3a1c8ca2be72c3067485
Thomas Azar <design@thomasazar.net>
2016-03-03 16:43:40 -0500

    Redirect logged in user to 'their' page

--- content/themes/DesignByThomasAzar/lib/myfunctions.php
+++ content/themes/DesignByThomasAzar/lib/myfunctions.php
@@ -7,4 +7,30 @@ function get_id_by_slug($page_slug) {
 		return null;
 	}
 }
+
+/**
+ * Redirect user after successful login.
+ *
+ * @param string $redirect_to URL to redirect to.
+ * @param string $request URL the user is coming from.
+ * @param object $user Logged user's data.
+ * @return string
+ */
+function my_login_redirect( $redirect_to, $request, $user ) {
+	//is there a user to check?
+	global $user;
+	if ( isset( $user->roles ) && is_array( $user->roles ) ) {
+		//check for admins
+		if ( in_array( 'administrator', $user->roles ) ) {
+			// redirect them to the default place
+			return $redirect_to;
+		} else {
+			return home_url( 'member-logged-in');
+		}
+	} else {
+		return $redirect_to;
+	}
+}
+
+add_filter( 'login_redirect', 'my_login_redirect', 10, 3 );
 ?>
