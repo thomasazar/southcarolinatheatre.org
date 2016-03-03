d251c2a9c70447a8566eee725cd4523e8ee12788
Thomas Azar <design@thomasazar.net>
2016-03-03 16:14:58 -0500

    Only show certain parts of the menu when logged in

--- content/themes/DesignByThomasAzar/nav.php
+++ content/themes/DesignByThomasAzar/nav.php
@@ -4,16 +4,40 @@
 		<?php get_search_form(); ?>
 	</div>
 	<ul class='header-menu-items'>
-		</div>
 		<?php
 
+		$exclude_array = [
+			'member-logged-in',
+			'member-logged-in/job-board',
+		];
+		$exclude = '';
+		foreach ( $exclude_array as $slug ) {
+			$exclude .= get_id_by_slug( $slug ) . ',';
+		}
+		
 		$args = array(
 			'title_li' => '',
 			'depth'    => '2',
 			'walker'   => new Clean_Walker_Page,
+			'exclude'  => $exclude,
 		);
 
 		wp_list_pages( $args );
+
+		if( is_user_logged_in() ){
+			global $current_user;
+			get_currentuserinfo();
+
+			$args = [
+				'title_li' => '',
+				'walker'   => new Clean_Walker_Page,
+				'include'  => $exclude,
+				'echo'     => false,
+			];
+			$nav = wp_list_pages( $args );
+			$nav = str_replace( 'Member Logged In', "$current_user->display_name", $nav );
+			echo $nav;
+		}
 
 		?>
 	</ul>
