058b5a6056fe71831b3f15cf226937d01b4cee32
Thomas Azar <design@thomasazar.net>
2016-03-03 16:18:49 -0500

    Get page ID by slug. Member logged in page.

---
+++ content/themes/DesignByThomasAzar/lib/myfunctions.php
@@ -0,0 +1,10 @@
+<?php
+function get_id_by_slug($page_slug) {
+	$page = get_page_by_path($page_slug);
+	if ($page) {
+		return $page->ID;
+	} else {
+		return null;
+	}
+}
+?>
---
+++ content/themes/DesignByThomasAzar/page-member-logged-in.php
@@ -0,0 +1,39 @@
+<?php
+
+if ( !is_user_logged_in() ) :
+	auth_redirect();
+else:
+get_header();
+?>
+<body>
+	<header class='header'>
+		<?php get_template_part( 'page-header' ); ?>
+		<?php get_template_part( 'nav' ); ?>
+	</header>
+	<main class='container'>
+		<article class='post container'>
+			<?php while ( have_posts() ) : the_post(); ?>
+			<h1 class='post-title'>Welcome, <?php echo $current_user->first_name; ?>!</h1>
+			<section class='post__content'>
+				<?php
+				$child_pages = get_pages( array( 'parent' => $post->ID, 'sort_column' => 'menu_order' ) );
+				if ( $child_pages ){
+					$output = '<div class=\'download\'>';
+					foreach ( $child_pages as $child_page ) {
+						$title = $child_page->post_title;
+						$href  = get_permalink( $child_page->ID );
+						$output .= "<a href='$href' class='category'>$title</a>";
+					}
+					$output .= '</div>';
+					echo $output;
+				}
+				?>
+				<?php the_content(); ?>
+			</section>
+			<?php endwhile; ?>
+		</article>
+	</main>
+	<?php get_footer(); ?>
+</body>
+</html>
+<?php endif; ?>
