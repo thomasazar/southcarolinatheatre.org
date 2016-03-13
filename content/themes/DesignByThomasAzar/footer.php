7951ee9303242867367e14b68e3c0ff1f7af707d
Thomas Azar <design@thomasazar.net>
2016-03-13 14:58:01 -0400

    SCTA logo as footer background

--- content/themes/DesignByThomasAzar/footer.php
+++ content/themes/DesignByThomasAzar/footer.php
@@ -1,13 +1,12 @@
 <footer class="footer">
 	<div class='container'>
-		<img class="footer__image" src='/img/scta-logo-small.png' />
 		<section class="footer__mission">
 			<p><strong>Mission:</strong>  To connect, create, and cultivate theatre in South Carolina.</p>
 			<p><strong>Vision:</strong>  To transform the lives of all South Carolinians through theatre.</p>
 			<p><strong>Purpose:</strong>  The purpose of the South Carolina Theatre Association is to bring together theatre groups – Educational Theatre, Community Theatre, Professional Theatre, and High School Theatre – to improve and further the cultural growth of theatrical art by mutual aid and advancement and stimulation of public interest.</p>
 		</section>
 		<section class='footer__contact'>
-			<p>Telephone: (864) 844-8111 | E-mail: SCTA2015@gmail.com</p>
+			<p>Telephone: (864) 844-8111 | E-mail: <a href='mailto:scta2015@gmail.com'>SCTA2015@gmail.com</a></p>
 		</section>
 	</div>
 </footer>
