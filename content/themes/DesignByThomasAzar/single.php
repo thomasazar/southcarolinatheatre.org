9c1f33ee2e634b899af836e9ca11436b863fa40b
Thomas Azar <design@thomasazar.net>
2016-03-13 15:00:15 -0400

    Put important info for divisions as an aside on the page

--- content/themes/DesignByThomasAzar/single.php
+++ content/themes/DesignByThomasAzar/single.php
@@ -8,15 +8,23 @@
 		<article class='post container'>
 			<?php while ( have_posts() ) : the_post(); ?>
 			<h1 class='post-title'><?php the_title(); ?></h1>
+            <aside class='aside'>
+                <?php // Get any uploads for division
+				$files = rwmb_meta( 'scta_uploads', 'type=file' );
+				foreach ( $files as $info ) {
+					echo "<a class='download-item' href='{$info['url']}' title='{$info['title']}'>{$info['title']}</a>";
+				}
+				$contact_name  = rwmb_meta( 'scta_division-chair-name' );
+				$contact_email = rwmb_meta( 'scta_division-chair-email' );
+				$contact_phone = rwmb_meta( 'scta_division-chair-phone' );
+                if ( $contact_name ): ?>
+                    <h3 class='aside-title'>Division Chair</h3>
+                    <p><?php echo $contact_name; ?></p>
+                    <p><a href='mailto:<?php echo $contact_email;?>'><?php echo $contact_email; ?></a></p>
+                    <p><?php foreach ( $contact_phone as $phone ) { echo "<p>$phone</p>"; } ?></p>
+                <?php endif; ?>
+				</aside>
 			<section class='post__content'>
-				<div class='download'>
-					<?php
-					$files = rwmb_meta( 'scta_cv', 'type=file' );
-					foreach ( $files as $info ) {
-						echo "<a class='download-item' href='{$info['url']}' title='{$info['title']}'>{$info['title']}</a>";
-					}
-					?>
-				</div>
 				<?php the_content(); ?>
 			</section>
 			<?php endwhile; ?>
