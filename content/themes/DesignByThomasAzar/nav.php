78bf4088134b6eabc1acdffcb3e3ddf7d5256c3f
Thomas Azar <design@thomasazar.net>
2016-03-14 16:03:39 -0400

    No longer exclude pages from nav

--- content/themes/DesignByThomasAzar/nav.php
+++ content/themes/DesignByThomasAzar/nav.php
@@ -5,40 +5,12 @@
 	</div>
 	<ul class='header-menu-items'>
 		<?php
-
-		$exclude_array = [
-			'member-logged-in',
-			'member-logged-in/job-board',
-		];
-		$exclude = '';
-		foreach ( $exclude_array as $slug ) {
-			$exclude .= get_id_by_slug( $slug ) . ',';
-		}
-		
-		$args = array(
-			'title_li' => '',
-			'depth'    => '2',
-			'walker'   => new Clean_Walker_Page,
-			'exclude'  => $exclude,
-		);
-
+		$args = [
+            'title_li' => '',
+            'depth'    => '2',
+            'walker'   => new Clean_Walker_Page,
+        ];
 		wp_list_pages( $args );
-
-		if( is_user_logged_in() ){
-			global $current_user;
-			get_currentuserinfo();
-
-			$args = [
-				'title_li' => '',
-				'walker'   => new Clean_Walker_Page,
-				'include'  => $exclude,
-				'echo'     => false,
-			];
-			$nav = wp_list_pages( $args );
-			$nav = str_replace( 'Member Logged In', "$current_user->display_name", $nav );
-			echo $nav;
-		}
-
 		?>
 	</ul>
 </nav>
