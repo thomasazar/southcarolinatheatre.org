a8637d5b36a48b3234df43f797b24a36192e91ea
Thomas Azar <design@thomasazar.net>
2016-03-20 12:55:00 -0400

    Need to style login button

--- content/themes/DesignByThomasAzar/nav.php
+++ content/themes/DesignByThomasAzar/nav.php
@@ -6,11 +6,15 @@
 	<ul class='header-menu-items'>
 		<?php
 		$args = [
-            'title_li' => '',
-            'depth'    => '2',
-            'walker'   => new Clean_Walker_Page,
-        ];
+      'title_li' => '',
+      'depth'    => '2',
+      'walker'   => new Clean_Walker_Page,
+      'exclude'  => get_id_by_slug('member-login'),
+    ];
 		wp_list_pages( $args );
 		?>
+    <li class='page_item page_item--login'>
+      <a href='/member-login'>Login</a>
+    </li>
 	</ul>
 </nav>
