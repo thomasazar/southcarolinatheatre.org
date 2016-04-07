3ebb4f897ab3ac1b5f709677af109ca93a9c94e6
Thomas Azar <design@thomasazar.net>
2016-04-07 18:28:58 -0400

    Put php_error.log in web root

--- web/local-config.php
+++ web/local-config.php
@@ -6,16 +6,16 @@ In it, you *must* include the four main database defines
 You may include other settings here that you only want enabled on your local development checkouts
 */
 
-define( 'DB_NAME', 'scta' );
-define( 'DB_USER', 'scta' );
-define( 'DB_PASSWORD', 'scta' );
+define( 'DB_NAME', 'southcarolinatheatre_local_development' );
+define( 'DB_USER', 'southcarolinatheatre_local' );
+define( 'DB_PASSWORD', 'southcarolinatheatre' );
 define( 'DB_HOST', 'localhost' ); // Probably 'localhost'
 
 define('FS_METHOD','direct');
 
 @ini_set('log_errors','On'); // enable or disable php error logging (use 'On' or 'Off')
 @ini_set('display_errors','On'); // enable or disable public display of errors (use 'On' or 'Off')
-@ini_set('error_log','/var/www/southcarolinatheatre.local/php_error.log'); // path to server-writable log file
+@ini_set('error_log', __DIR__ . '/php_error.log'); // path to server-writable log file
 
 define( 'SAVEQUERIES', true );
 define( 'WP_DEBUG', true );
