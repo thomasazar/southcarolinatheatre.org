2eb122e7460df1f7e1536a2991cd5e21fe9e64ea
Thomas Azar <design@thomasazar.net>
2016-07-02 13:39:36 -0400

    Correct verbiage, add Piwik

--- web/content/themes/DesignByThomasAzar/footer.php
+++ web/content/themes/DesignByThomasAzar/footer.php
@@ -3,12 +3,26 @@
 		<section class="footer__mission">
 			<p><strong>Mission:</strong>  To connect, create, and cultivate theatre in South Carolina.</p>
 			<p><strong>Vision:</strong>  To transform the lives of all South Carolinians through theatre.</p>
-			<p><strong>Purpose:</strong>  The purpose of the South Carolina Theatre Association is to bring together theatre groups – Educational Theatre, Community Theatre, Professional Theatre, and High School Theatre – to improve and further the cultural growth of theatrical art by mutual aid and advancement and stimulation of public interest.</p>
+			<p><strong>Purpose:</strong>  To bring together theatre groups – Educational Theatre, Community Theatre, Professional Theatre, and High School Theatre – to improve and further the cultural growth of theatrical art by mutual aid and advancement and stimulation of public interest.</p>
 		</section>
 		<section class='footer__contact'>
 			<p>Telephone: (864) 844-8111 | E-mail: <a href='mailto:scta2015@gmail.com'>SCTA2015@gmail.com</a></p>
 		</section>
 	</div>
 </footer>
-
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//piwik.thomasazar.net/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 7]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="//piwik.thomasazar.net/piwik.php?idsite=7" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
 <?php wp_footer(); ?>
