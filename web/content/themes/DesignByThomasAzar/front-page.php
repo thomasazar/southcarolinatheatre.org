9402049917e17b728fc1e35c359830d3db7afc77
Thomas Azar <design@thomasazar.net>
2016-07-05 11:11:32 -0400

    Remove Countdown clock

--- web/content/themes/DesignByThomasAzar/front-page.php
+++ web/content/themes/DesignByThomasAzar/front-page.php
@@ -51,49 +51,6 @@ $query = new WP_Query( $args );
         </article>
         <?php wp_reset_postdata(); endwhile; endif; ?>
     </main>
-    <script>
-    function getTimeRemaining(endtime) {
-        var t = Date.parse(endtime) - Date.parse(new Date());
-        var seconds = Math.floor((t / 1000) % 60);
-        var minutes = Math.floor((t / 1000 / 60) % 60);
-        var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-        var days = Math.floor(t / (1000 * 60 * 60 * 24));
-        return {
-            'total': t,
-            'days': days,
-            'hours': hours,
-            'minutes': minutes,
-            'seconds': seconds
-        };
-    }
-
-    function initializeClock(id, endtime) {
-        var clock = document.getElementById(id);
-        var daysSpan = clock.querySelector('.timer__days');
-        var hoursSpan = clock.querySelector('.timer__hours');
-        var minutesSpan = clock.querySelector('.timer__minutes');
-        var secondsSpan = clock.querySelector('.timer__seconds');
-
-        function updateClock() {
-            var t = getTimeRemaining(endtime);
-
-            daysSpan.innerHTML = t.days;
-            hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-            minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-            secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-            if (t.total <= 0) {
-                clearInterval(timeinterval);
-            }
-        }
-
-        updateClock();
-        var timeinterval = setInterval(updateClock, 1000);
-    }
-
-    var deadline = 'May 14 2016 19:00:00 GMT-05:00';
-    initializeClock('timer', deadline);
-    </script>
     <?php get_footer(); ?>
 </body>
 </html>
