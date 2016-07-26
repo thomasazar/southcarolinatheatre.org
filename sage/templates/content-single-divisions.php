396e97e4ae04123361cbfbc0470c63a3fb6a2086
Thomas Azar <design@thomasazar.net>
2016-07-26 17:02:08 -0400

    Margins for post__content

--- sage/templates/content-single-divisions.php
+++ sage/templates/content-single-divisions.php
@@ -5,12 +5,12 @@
       <h4 class='edit-this-page'><?php edit_post_link( 'Edit this page' ); ?></h4>
     <?php endif; ?>
     <?php get_template_part('templates/aside'); ?>
+    <?php if ( has_post_thumbnail() ) : ?>
+      <section class="byline">
+        <?php the_post_thumbnail('thumbnail', ['class' => 'byline__image',]); ?>
+        <p class="byline__author"><?php echo get_post(get_post_thumbnail_id())->post_title; //The Title ?></p>
+      </section>
     <section class="post__content">
-      <?php if ( has_post_thumbnail() ) : ?>
-        <section class="byline">
-          <?php the_post_thumbnail('thumbnail', ['class' => 'byline__image',]); ?>
-          <p class="byline__author"><?php echo get_post(get_post_thumbnail_id())->post_title; //The Title ?></p>
-        </section>
       <?php endif; ?>
       <?php the_content(); ?>
     </section>
