d24e345a4d58e1ec10ae663f17e21a13dd81000e
Thomas Azar <design@thomasazar.net>
2016-07-28 12:10:52 -0400

    Call for child pages using WP_Query

---
+++ sage/page-member-login.php
@@ -0,0 +1,18 @@
+<article class='post container'>
+  <?php while ( have_posts() ) : the_post(); ?>
+    <section class='post__content'>
+      <?php
+      if (is_user_logged_in()) :
+        if ( $current_user->user_firstname ) {
+          $greeting = 'Welcome, ' . $current_user->user_firstname . '!';
+        } else {
+          $greeting = 'Welcome!';
+        }
+        ?>
+        <h1 class='post__title'><?= $greeting; ?></h1>
+        <?php get_template_part('templates/child-pages'); ?>
+      <?php endif; ?>
+      <?php the_content(); ?>
+    </section>
+  <?php endwhile; ?>
+</article>
--- sage/templates/content-page.php
+++ sage/templates/content-page.php
@@ -1,19 +1,7 @@
 <article class='post'>
   <?php get_template_part('templates/page', 'header'); ?>
   <section class='post__content'>
-    <?php
-    $child_pages = get_pages( array( 'parent' => $post->ID, 'sort_column' => 'menu_order' ) );
-    if ( $child_pages ){
-      $output = '<div class=\'categories\'>';
-      foreach ( $child_pages as $child_page ) {
-        $title = $child_page->post_title;
-        $href  = get_permalink( $child_page->ID );
-        $output .= "<a href='$href' class='category'>$title</a>";
-      }
-      $output .= '</div>';
-      echo $output;
-    }
-    ?>
+    <?php get_template_part('templates/child-pages'); ?>
     <?php the_content(); ?>
   </section>
 </article>
