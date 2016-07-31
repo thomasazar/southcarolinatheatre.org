a0e6fa46195e7cc4db008be4f03a05eb960437f5
Thomas Azar <design@thomasazar.net>
2016-07-31 13:35:32 -0400

    Don't include hero post type in search results.

--- sage/lib/post-types/hero.php
+++ sage/lib/post-types/hero.php
@@ -44,7 +44,7 @@ function scta_hero() {
 		'show_in_nav_menus'     => true,
 		'can_export'            => true,
 		'has_archive'           => false,
-		'exclude_from_search'   => false,
+		'exclude_from_search'   => true,
 		'publicly_queryable'    => true,
 		'capability_type'       => 'page',
 	);
