4650db08a3e9a5a2cbf3e0e3635bf014923f6e53
Thomas Azar <design@thomasazar.net>
2016-08-25 15:36:17 -0400

    Open Graph protocol in <head>

--- web/app/themes/sage/templates/head.php
+++ web/app/themes/sage/templates/head.php
@@ -2,6 +2,10 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta property="og:title" content="South Carolina Theatre Association">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="<?php echo get_permalink($post->ID); ?>">
+  <meta property="og:image" content="<?php echo get_site_icon_url(); ?>">
   <link rel="author" href="/humans.txt" />
   <?php wp_head(); ?>
 </head>
