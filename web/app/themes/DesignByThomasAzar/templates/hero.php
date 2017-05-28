7a6b0b6849e667bf08496311dce88954f780f315
Thomas Azar <design@thomasazar.net>
2017-05-28 11:50:20 -0400

    Simplify

--- web/app/themes/DesignByThomasAzar/templates/hero.php
+++ web/app/themes/DesignByThomasAzar/templates/hero.php
@@ -1,29 +1,13 @@
-<?php
-  $args = [
-    'posts_per_page' => 1,
-    'post_type' => 'scta_hero',
-    'post_status' => 'publish',
-    'orderby' => 'date',
-    'order' => 'DESC',
-  ];
-
-  $the_query = new WP_Query($args);
-?>
-<?php if ($the_query->have_posts()) : ?>
-  <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
-    <article class='hero'>
-      <?php $hero__link = rwmb_meta($field_id = 'scta_link-address', $post_id = $the_query->ID); ?>
-      <a class="hero__link" href="<?php echo esc_url(get_permalink($hero__link)); ?>">
-      <div class='container'>
-        <section class='hero__text'>
-          <h2 class='hero__headline'><?php the_excerpt(); ?></h2>
-        </section>
-        <section class='hero__focus'>
-          <?php the_post_thumbnail('large', ['class' => 'hero__img',]); ?>
-        </section>
-      </div>
+<?php $image = get_field('image'); ?>
+<article class="hero">
+    <a class="hero__link" href="<?php the_field('link'); ?>">
+        <div class="container">
+            <section class="hero__text">
+                <h2 class="hero__headline"><?php the_field('text'); ?></h2>
+            </section>
+            <section class="hero__focus">
+                <img src="<?= $image['sizes']['large']; ?>" class="hero__img">
+            </section>
+        </div>
     </a>
-    </article>
-    <?php wp_reset_postdata(); ?>
-  <?php endwhile; ?>
-<?php endif;?>
+</article>
