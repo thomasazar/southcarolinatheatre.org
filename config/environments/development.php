f3710ba03ec2a70453d7b07479bb9455913b64f7
Thomas Azar <design@thomasazar.net>
2017-11-13 14:49:07 -0500

    Adhere to Bedrock

--- config/environments/development.php
+++ config/environments/development.php
@@ -1,7 +1,5 @@
 <?php
-@ini_set('display_errors','On'); // enable or disable public display of errors (use 'On' or 'Off')
-
-define('SAVEQUERIES', true );
-define('WP_DEBUG', true );
-define('LOCALHOST', true);
+/** Development */
+define('SAVEQUERIES', true);
+define('WP_DEBUG', true);
 define('SCRIPT_DEBUG', true);
--- web/index.php
+++ web/index.php
@@ -1,4 +1,5 @@
 <?php
-// WordPress view bootstrapper
-define( 'WP_USE_THEMES', true );
-require( './wp/wp-blog-header.php' );
+
+/** WordPress view bootstrapper */
+define('WP_USE_THEMES', true);
+require(__DIR__ . '/wp/wp-blog-header.php');
