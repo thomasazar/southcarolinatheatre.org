385c01a085abe583171477b21825e18ad6663c94
Thomas Azar <design@thomasazar.net>
2017-12-05 15:37:50 -0500

    Remove Google fonts

--- web/app/themes/DesignByThomasAzar/templates/head.php
+++ web/app/themes/DesignByThomasAzar/templates/head.php
@@ -2,6 +2,5 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://fonts.googleapis.com/css?family=Arvo" rel="stylesheet">
   <?php wp_head(); ?>
 </head>
