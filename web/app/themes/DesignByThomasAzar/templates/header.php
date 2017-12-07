d85e073043d4b35f1f1c48896814cce80cb9c029
Thomas Azar <design@thomasazar.net>
2017-12-07 15:41:49 -0500

    Set up so flexbox is in charge of ordering the menus.

--- web/app/themes/DesignByThomasAzar/templates/header.php
+++ web/app/themes/DesignByThomasAzar/templates/header.php
@@ -3,15 +3,22 @@
 <?php $image = get_field('image', $frontpage); ?>
 <?php $background = $image['sizes']['large']; ?>
 
-<header class="banner" style="background-image: linear-gradient(to bottom, rgba(17,83,115,0.5) 0%,rgba(17,83,115,0.5) 100%), url('<?= $background; ?>');">
-  <nav class="banner__nav">
-    <?php get_menu('secondary', 'nav-menu nav-menu--secondary'); ?>
-    <?php get_menu('primary', 'nav-menu nav-menu--primary'); ?>
-  </nav>
+<header class="banner" style="background-image: linear-gradient(to bottom, rgba(0,103,140,0.75) 0%, rgba(61,170,213,0.5) 100%), url('<?= $background; ?>');">
+    <section class="mobile-only mobile-menu">
+      <?php $svg  = get_template_directory_uri() . '/dist/images/logo-side-text.svg'; ?>
+      <a class="logo__link mobile-menu__logo" href="<?= esc_url(home_url('/')); ?>">
+        <object class="logo" type="image/svg+xml" data="<?= $svg; ?>">SCTA</object>
+      </a>
+      <a class="mobile-menu__button">Menu</a>
+    </section>
+    <section class="banner__menus">
+      <?php get_menu('primary', 'nav-menu nav-primary'); ?>
+      <?php get_menu('secondary', 'nav-menu nav-secondary'); ?>
+    </section>
   <?php if (is_front_page()) : ?>
     <section class="tagline">
       <h1><?= bloginfo('description'); ?></h1>
-      <p>SCTA's vision is to transform the lives<br>of all South Carolinians through theatre</p>
+      <p>SCTA's vision is to transform the lives of all South Carolinians through theatre</p>
     </section>
   <?php endif; ?>
 </header>
