07991f8b153d45786c084ea431e5bca1aac3f9e0
Thomas Azar <design@thomasazar.net>
2017-12-13 12:53:40 -0500

    Don't show in forum posts

--- web/app/themes/DesignByThomasAzar/templates/aside.php
+++ web/app/themes/DesignByThomasAzar/templates/aside.php
@@ -1,5 +1,8 @@
+<?php $show_only = ['convention', 'divisions', 'events', 'page' ]; ?>
 <aside class="aside">
-  <?php get_template_part('templates/aside', 'attachments'); ?>
-  <?php get_template_part('templates/aside', 'children'); ?>
-  <?php get_template_part('templates/aside', 'links'); ?>
+  <?php if (in_array(get_post_type(), $show_only)) : ?>
+    <?php get_template_part('templates/aside', 'attachments'); ?>
+    <?php get_template_part('templates/aside', 'children'); ?>
+    <?php get_template_part('templates/aside', 'links'); ?>
+  <?php endif; ?>
 </aside>
