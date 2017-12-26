1cbda4ccb133df7d429d186fb6a41867906012a4
Thomas Azar <design@thomasazar.net>
2017-12-26 15:37:58 -0500

    Piwik Analytics

--- web/app/themes/DesignByThomasAzar/templates/head.php
+++ web/app/themes/DesignByThomasAzar/templates/head.php
@@ -2,5 +2,21 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Piwik -->
+  <script type="text/javascript">
+  var _paq = _paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//piwik.thomasazar.net/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', '3']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+  </script>
+  <noscript><p><img src="//piwik.thomasazar.net/piwik.php?idsite=3&rec=1" style="border:0;" alt="" /></p></noscript>
+  <!-- End Piwik Code -->
   <?php wp_head(); ?>
 </head>
