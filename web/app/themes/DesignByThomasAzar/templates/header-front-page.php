8828c84e8a4222800724e745ff0ba29d6cf38fc4
Thomas Azar <design@thomasazar.net>
2018-01-22 11:57:26 -0500

    More info for home page hero image

--- web/app/themes/DesignByThomasAzar/templates/header-front-page.php
+++ web/app/themes/DesignByThomasAzar/templates/header-front-page.php
@@ -1,23 +1,28 @@
 <?php use function DesignByThomasAzar\Functions\get_menu; ?>
-<?php $image = get_field('image'); ?>
-<?php $background = $image['sizes']['large']; ?>
+<?php $hero = get_field('hero'); ?>
+<?php $background = $hero['image']['sizes']['large']; ?>
 
 <header class="banner" style="background-image: linear-gradient(to bottom, rgba(0,103,140,0.75) 0%, rgba(61,170,213,0.5) 100%), url('<?= $background; ?>');">
-    <section class="mobile-only mobile-menu">
-      <?php $svg  = get_template_directory_uri() . '/dist/images/logo-side-text.svg'; ?>
-      <a class="logo__link mobile-menu__logo" href="<?= esc_url(home_url('/')); ?>">
-        <object class="logo" type="image/svg+xml" data="<?= $svg; ?>">SCTA</object>
+  <section class="mobile-only mobile-menu">
+    <?php $svg  = get_template_directory_uri() . '/dist/images/logo-side-text.svg'; ?>
+    <a class="logo__link mobile-menu__logo" href="<?= esc_url(home_url('/')); ?>">
+      <object class="logo" type="image/svg+xml" data="<?= $svg; ?>">SCTA</object>
+    </a>
+    <a class="mobile-menu__button">Menu</a>
+  </section>
+  <section class="banner__menus">
+    <?php get_menu('primary', 'nav-menu nav-primary'); ?>
+    <?php get_menu('secondary', 'nav-menu nav-secondary'); ?>
+  </section>
+  <section class="tagline">
+    <h1><?= bloginfo('description'); ?></h1>
+    <p>SCTA's vision is to transform the lives of all South Carolinians through theatre</p>
+  </section>
+  <?php if ($hero['caption']) : ?>
+    <section class="photo-credit">
+      <a class="photo-credit__link" href="<?= $hero['image']['url']; ?>">
+        <?= $hero['caption']; ?>
       </a>
-      <a class="mobile-menu__button">Menu</a>
-    </section>
-    <section class="banner__menus">
-      <?php get_menu('primary', 'nav-menu nav-primary'); ?>
-      <?php get_menu('secondary', 'nav-menu nav-secondary'); ?>
-    </section>
-  <?php if (is_front_page()) : ?>
-    <section class="tagline">
-      <h1><?= bloginfo('description'); ?></h1>
-      <p>SCTA's vision is to transform the lives of all South Carolinians through theatre</p>
     </section>
   <?php endif; ?>
 </header>
