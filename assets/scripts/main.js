0ff2031383ca87fa4ac97c872c4b7254ec7ab566
Thomas Azar <design@thomasazar.net>
2019-08-23 09:14:34 -0400

    Removing domReady function seems to fix the menu

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,23 +1,10 @@
-function domReady(fn) {
-  // If we're early to the party
-  document.addEventListener("DOMContentLoaded", fn);
-  // If late; I mean on time.
-  if (document.readyState === "interactive" || document.readyState === "complete" ) {
-    fn();
+var navOpen, navClose;
+navButton = document.querySelector('#mobile-menu__button');
+navButton.addEventListener('click', function () {
+  document.querySelector('.navigation').classList.toggle('open');
+  if (navButton.innerHTML === 'Menu') {
+    navButton.innerHTML = 'Close'
+  } else {
+    navButton.innerHTML = 'Menu';
   }
-}
-
-domReady(nav_menu);
-
-function nav_menu() {
-  var navOpen, navClose;
-  navButton = document.querySelector('#mobile-menu__button');
-  navButton.addEventListener('click', function(){
-    document.querySelector('.navigation').classList.toggle('open');
-    if (navButton.innerHTML === 'Menu') {
-      navButton.innerHTML = 'Close'
-    } else {
-      navButton.innerHTML = 'Menu';
-    }
-  });
-}
+});
